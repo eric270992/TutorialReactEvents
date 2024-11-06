8adabc2aee49ca9fbbd0f88f10b36aea53049972
Eric Clota Ortiz <eclota@eio.cat>
2024-11-06 11:42:11 +0100

    Part 3 i 4

---
+++ src/App4.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+export default function FeedbackForm() {
+
+  function handleClick() {
+    const name = prompt('¿Cuál es tu nombre?');
+    alert(`Hola, ${name}!`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Saludar
+    </button>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./styles.css";
 
 import App from "./App";
 import App2 from "./App2";
-
+import App3 from "./App3";
+import App4 from "./App4";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
     <App2 />
+    <App3 />
+    <App4 />
   </StrictMode>
 );
