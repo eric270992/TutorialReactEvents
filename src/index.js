31073697b0214c59a4bc383b2df068fab515f08d
Eric Clota Ortiz <eclota@eio.cat>
2024-11-06 11:33:08 +0100

    Resolt segona part

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App from "./App";
+import App2 from "./App2";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
+    <App2 />
   </StrictMode>
 );
